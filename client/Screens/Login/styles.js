38278fad307e4acdbb49d7e9713a454b71d2da5f
jjonb <jjonbld@gmail.com>
2022-05-18 11:51:07 -0700

    login icons

--- client/Screens/Login/styles.js
+++ client/Screens/Login/styles.js
@@ -9,13 +9,17 @@ const styles = StyleSheet.create({
 
   textInput: {
     height: 50,
-    width: 350,
+    width: 325,
     borderRadius: 10,
     backgroundColor: "#F9F9FB",
     textAlign: "center",
     borderWidth: 1,
     marginBottom: 20,
     borderRadius: 10,
+    paddingTop: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    paddingLeft: 0,
   },
   button: {
     alignItems: "center",
