086882e331cb2f1967e4f5df392c1658ef3ec3a5
jjonb <jjonbld@gmail.com>
2022-05-23 16:53:57 -0700

    edited public route

---
+++ client/Screens/NewBlog/NewBlog.js
@@ -0,0 +1,60 @@
+import {
+  View,
+  Text,
+  Pressable,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  FlatList,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+import styles from "./styles.js";
+
+const NewBlog = (props) => {
+  const [subject, setSubject] = useState("");
+  const [text, setText] = useState("");
+  let UrlString = "localhost";
+
+  // "10.0.2.2" is needed if this request is coming from physical device
+  if (Platform.OS == "android") {
+    UrlString = "10.0.2.2";
+  }
+
+  const createPost = async () => {
+    await axios.post(
+      `http://${UrlString}:5050/blog/new`,
+      { subject: subject, text: text },
+      {
+        headers: { "x-auth-token": props.token },
+      }
+    );
+
+    setSubject("");
+    setText("");
+    props.navigation.goBack();
+  };
+
+  return (
+    <View>
+      <TextInput
+        style={styles.subjectInput}
+        placeholder="Subject"
+        onChangeText={setSubject}
+        value={subject}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Write your Blog"
+        onChangeText={setText}
+        value={text}
+      />
+      <TouchableOpacity onPress={createPost}>
+        <Text style={styles.button}>Post</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default NewBlog;
---
+++ client/Screens/NewBlog/styles.js
@@ -0,0 +1,66 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  blogContainer: {
+    flex: 1,
+  },
+  subjectInput: {
+    borderColor: "white",
+    width: 360,
+    height: 50,
+    padding: 4,
+    paddingLeft: 24,
+    borderRadius: 10,
+    marginTop: 20,
+    backgroundColor: "white",
+    fontSize: 18,
+    marginHorizontal: 20,
+  },
+  textInput: {
+    borderColor: "white",
+    width: 360,
+    height: 200,
+    padding: 4,
+    paddingLeft: 24,
+    borderRadius: 10,
+    margin: 20,
+    backgroundColor: "white",
+    fontSize: 18,
+  },
+  button: {
+    color: "white",
+    fontSize: 20,
+    backgroundColor: "#12a6e6",
+    borderRadius: 10,
+    width: 70,
+    marginLeft: 50,
+    textAlign: "center",
+  },
+  logoutButton: {
+    color: "white",
+    fontSize: 20,
+    backgroundColor: "#12a6e6",
+    borderRadius: 10,
+    width: 80,
+    marginLeft: 300,
+    textAlign: "center",
+    marginRight: 100,
+    marginTop: 100,
+  },
+  logout: {
+    backgroundColor: "#ebf1fa",
+    fontSize: 18,
+    height: 400,
+    width: 400,
+    borderRadius: 20,
+    alignItems: "center",
+    shadowOffset: { width: -7, height: 8 },
+    shadowOpacity: 0.2,
+    justifyContent: "center",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: 200,
+  },
+});
+
+export default styles;
